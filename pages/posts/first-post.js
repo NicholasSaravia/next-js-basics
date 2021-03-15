d74ef721fae26e9a2ee5fb51f646f52307ed9598
Nick Saravia <nick.saravia@gmail.com>
2021-03-15 15:35:40 -0400

    Assests, Metadata, and CSS - 9

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from "next/image";
 import Head from 'next/head';
-import { Layout } from '../../components/layout';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
@@ -10,17 +10,7 @@ export default function FirstPost() {
             <title>First Post</title>
         </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-      <Image
-        src="/images/nick.jpg" // Route of the image file
-        height={144} // Desired size with correct aspect ratio
-        width={144} // Desired size with correct aspect ratio
-        alt="Your Name"
-      />
+     
     </Layout>
   );
 }
